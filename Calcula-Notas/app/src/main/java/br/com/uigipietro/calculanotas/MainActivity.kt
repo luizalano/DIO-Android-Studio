4c35067221a05cf5eebe5bd6da815c222b33a463
Luiz Alberto Alano <alano@luigipietro.com.br>
2021-02-27 20:45:14 -0300

    Commit inicial

---
+++ Calcula-Notas/app/src/main/java/br/com/uigipietro/calculanotas/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.uigipietro.calculanotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
